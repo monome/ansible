a88333ad79a78dc4e85975a544cec42fae56dcc9
Sam Boling <charles.samuel.boling@gmail.com>
2018-11-30 20:56:20 -0700

    don't think automated doc def extraction is gonna work

--- tools/extract_presets/docdef_writer.py
+++ tools/extract_presets/docdef_writer.py
@@ -16,6 +16,7 @@ class DocdefWriter:
     def __init__(self, version):
         self.indentation = 0
         self.obj_depth = -1
+        self.arr_depth = -1
         self.ffi = FFI()
         try:
             self.schema = PRESET_SCHEMATA[version](self.ffi)
@@ -48,9 +49,11 @@ class DocdefWriter:
         self.write_object(out, nvram_t)
         out.write(';\n')
 
-    def write_object(self, out, ctype):
+    def write_object(self, out, ctype, name=None):
         self.obj_depth += 1
         with self.indent(out, ('{', '}')):
+            if name is not None:
+                self.putln(out, '.name = {},'.format(name))
             self.putln(out, '.read = load_object,')
             self.putln(out, '.read = save_object,')
             self.putln(out, '.fresh = true,')
@@ -58,8 +61,113 @@ class DocdefWriter:
                 out,
                 '.state = &json_object_state[{}],'.format(self.obj_depth),
             )
-            
-            self.put_indented(out, '.params = &')
-            with self.indent(out, ('((load_object_params_t) {', '}),\n')):
+
+            with self.write_params(out, 'load_object_params_t'):
                 self.putln(out, '.handler_ct = {},'.format(len(ctype.fields)))
+                self.put_indented(out, '.handlers = ')
+                with self.indent(
+                    out,
+                    (
+                        '((preset_section_handler_t[]) {',
+                        '}),\n'
+                    )
+                ):
+                    for field_name, field in ctype.fields:
+                        kind_writer = self.get_writer(field.type.kind)
+                        self.put_indented(out, '')
+                        with self.indent(out, ('{', '},\n')):
+                            self.putln(out, '.name = "{}",'.format(field_name))
+                            kind_writer(out, field_name, field)
+
         self.obj_depth -= 1
+
+    def get_writer(self, kind):
+        return getattr(self, 'write_{}'.format(kind))
+
+    def write_primitive(self, out, field_name, field):
+        self.putln(out, '.read = load_scalar,')
+        if field.type.cname.startswith('uint'):
+            self.putln(out, '.write = save_number,')
+            with self.write_params(out, 'load_scalar_params_t', field_name):
+                pass
+            return
+        if field.type.cname.startswith('int'):
+            self.putln(out, '.write = save_number,')
+            with self.write_params(out, 'load_scalar_params_t', field_name):
+                self.putln('.signed_val = true,')
+            return
+        if field.type.cname == '_Bool':
+            self.putln(out, '.write = save_bool,')
+            with self.write_params(out, 'load_scalar_params_t', field_name):
+                pass
+            return
+        import pdb; pdb.set_trace()
+
+    def write_enum(self, out, field_name, field):
+        # FIXME
+        pass
+
+    def write_array(self, out, field_name, field):
+        self.arr_depth += 1
+        
+        self.putln(out, '.read = load_array,')
+        self.putln(out, '.write = save_array,')
+        self.putln(out, '.fresh = true,')
+        self.putln(out, '.state = &json_array_state[{}],'.format(self.arr_depth))
+        with self.write_params(out, 'load_array_params_t'):
+            self.putln(
+                out,
+                '.array_len = sizeof_field({root_t}, {name}) / sizeof_field({root_t}, {name}[0]),'.format(
+                    root_t=self.ROOT_TYPE,
+                    name=field_name,
+                ),
+            )
+            self.putln(
+                out,
+                '.item_size = sizeof_field({}, {}[0]),'.format(
+                    self.ROOT_TYPE,
+                    field_name,
+                )
+            )
+            self.put_indented(out, '.item_handler = ')
+            with self.indent(
+                out,
+                (
+                    '((preset_section_handler_t) {',
+                    '}),\n'
+                )
+            ):
+                kind_writer = self.get_writer(field.type.kind)
+
+            
+        self.arr_depth -= 1
+        
+    def write_struct(self, out, field_name, field):
+        self.write_object(out, field.type, field_name)
+        
+    @contextmanager
+    def write_params(self, out, params_t, field_name=None):
+        self.put_indented(out, '.params = &')
+        with self.indent(
+            out,
+            (
+                '(({}) {{'.format(params_t),
+                '}),\n'
+            )
+        ):
+            if field_name is not None:
+                self.putln(
+                    out,
+                    '.dst_offset = offsetof({}, state.{}),'.format(
+                        self.ROOT_TYPE,
+                        field_name,
+                    )
+                )
+                self.putln(
+                    out,
+                    '.dst_size = sizeof_field({}, state.{}),'.format(
+                        self.ROOT_TYPE,
+                        field_name,
+                    )
+                )
+            yield
