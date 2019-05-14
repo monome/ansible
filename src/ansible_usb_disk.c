37cbeec9a46888b294a1f40ac64bf6af7eeebfef
Sam Boling <charles.samuel.boling@gmail.com>
2019-05-13 22:14:55 -0600

    implement usb disk UI

--- src/ansible_usb_disk.c
+++ src/ansible_usb_disk.c
@@ -19,8 +19,10 @@
 #include "uhi_msc_mem.h"
 
 #define DEBUG_ANSIBLE_USB_DISK 0
+#define DISK_BLINK_INTERVAL 250
 
 static void handler_UsbDiskKey(int32_t data);
+static void handler_UsbDiskFront(int32_t data);
 
 static bool usb_disk_mount_drive(void);
 static bool usb_disk_backup_binary(FS_STRING fname);
@@ -28,17 +30,18 @@ static bool usb_disk_restore_backup(FS_STRING fname);
 static bool usb_disk_load_flash(FS_STRING fname);
 static bool usb_disk_save_flash(FS_STRING fname);
 static void flush(void);
+static void blink_read(void* o);
+static void blink_write(void* o);
 
 static char ansible_usb_disk_textbuf[ANSIBLE_USBDISK_TXTBUF_LEN] = {  0 };
 static jsmntok_t ansible_usb_disk_tokbuf[ANSIBLE_USBDISK_TOKBUF_LEN];
 
 static volatile bool usb_disk_locked = false;
 
-static bool usb_disk_lock(uint8_t leds) {
+static bool usb_disk_lock(void) {
 	if (!usb_disk_locked) {
 		usb_disk_locked = true;
-		update_leds(leds);
-		print_dbg("\r\n\r\n\r\n> usb disk locked");
+		print_dbg("\r\n\r\n> usb disk locked");
 		return true;
 	}
 	return false;
@@ -46,35 +49,77 @@ static bool usb_disk_lock(uint8_t leds) {
 
 static void usb_disk_unlock(void) {
 	usb_disk_locked = false;
-	usb_disk_exit();
-	usb_disk_enter();
-	update_leds(0);
-	print_dbg("\r\n> usb disk unlocked\r\n\r\n");
+	print_dbg("\r\n> usb disk unlocked\r\n");
 }
 
+static volatile bool armed = false;
+static bool blink = false;
+
 static void handler_UsbDiskKey(int32_t data) {
+	bool success;
+
 	switch (data) {
 	case 0:
 		break;
 	case 1:
 		// key 1 - load
-		if (usb_disk_lock(2)) {
+		if (usb_disk_lock()) {
+			if (!armed) {
+				update_leds(1);
+				armed = true;
+				usb_disk_unlock();
+				return;
+			}
+			armed = false;
+			blink = false;
+			success = false;
+			timer_add(&auxTimer[0], DISK_BLINK_INTERVAL, &blink_read, NULL);
+
+			usb_disk_enter();
 			if (usb_disk_backup_binary(ANSIBLE_BACKUP_FILE)) {
 				if (usb_disk_load_flash(ANSIBLE_PRESET_FILE)) {
-				        flash_unfresh();
-					load_flash_state();
+					success = true;
 				} else {
 					usb_disk_restore_backup(ANSIBLE_BACKUP_FILE);
 				}
 			}
+			usb_disk_exit();
+
 			usb_disk_unlock();
+			timer_remove(&auxTimer[0]);
+			update_leds(0);
+			if (success) {
+				flash_unfresh();
+				load_flash_state();
+			} else {
+				update_leds(3);
+			}
 		}
 		break;
 	case 3:
 		// key 2 - save
-		if (usb_disk_lock(1)) {
-			usb_disk_save_flash(ANSIBLE_PRESET_FILE);
+		if (usb_disk_lock()) {
+			if (!armed) {
+				update_leds(2);
+		       		armed = true;
+				usb_disk_unlock();
+	       			return;
+       			}
+			armed = false;
+			blink = false;
+			success = false;
+			timer_add(&auxTimer[0], DISK_BLINK_INTERVAL, &blink_write, NULL);
+
+			usb_disk_enter();
+		        success = usb_disk_save_flash(ANSIBLE_PRESET_FILE);
+			usb_disk_exit();
+
 			usb_disk_unlock();
+			timer_remove(&auxTimer[0]);
+			update_leds(0);
+			if (!success) {
+				update_leds(3);
+			}
 		}
 		break;
 	default:
@@ -82,10 +127,28 @@ static void handler_UsbDiskKey(int32_t data) {
 	}
 }
 
+static void handler_UsbDiskFront(s32 data) {
+	if (usb_disk_lock()) {
+		armed = false;
+		update_leds(0);
+		usb_disk_unlock();
+	}
+}
+
+static void blink_read(void* o) {
+  update_leds(1 * blink);
+  blink = !blink;
+}
+
+static void blink_write(void* o) {
+  update_leds(2 * blink);
+  blink = !blink;
+}
+
 void set_mode_usb_disk(void) {
 	update_leds(0);
-	usb_disk_enter();
 	app_event_handlers[kEventKey] = &handler_UsbDiskKey;
+	app_event_handlers[kEventFront] = &handler_UsbDiskFront;
 }
 
 void usb_disk_enter() {
@@ -255,14 +318,14 @@ static bool usb_disk_load_flash(FS_STRING fname) {
 
 	switch (result) {
 	case JSON_READ_OK:
-	  print_dbg("\r\n> disk load successful");
-	  break;
+		print_dbg("\r\n> disk load successful");
+	  	break;
 	case JSON_READ_MALFORMED:
-	  print_dbg("\r\n> disk backup malformed");
-	  break;
+		print_dbg("\r\n> disk backup malformed");
+		break;
 	default:
-	  print_dbg("\r\n> reached invalid state");
-	  break;
+		print_dbg("\r\n> reached invalid state");
+		break;
 	}
 	print_dbg("\r\n> usb disk load done");
 
@@ -273,17 +336,16 @@ static bool usb_disk_save_flash(FS_STRING fname) {
 	print_dbg("\r\n> writing flash to disk");
 	if (!nav_file_create(fname)) {
 		if (fs_g_status != FS_ERR_FILE_EXIST) {
-  print_dbg("\r\n!! could not create file");
+			print_dbg("\r\n!! could not create file");
 			return false;
 		}
 	}
 	if (!file_open(FOPEN_MODE_W)) {
-  print_dbg("\r\n!! could not open file");
+		print_dbg("\r\n!! could not open file");
 		return false;
 	}
 	total_written = 0;
 	json_write_result_t result = json_write(
-		/* puts_chunks, */
 		puts_buffered,
 		(void*)&f, &ansible_preset_docdef);
 	flush();
