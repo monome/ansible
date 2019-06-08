54f191a3e87183cc13571065eda3efef94681927
Sam Boling <charles.samuel.boling@gmail.com>
2019-06-08 16:09:04 -0600

    remove commented PREBUILD_CMD

--- src/config.mk
+++ src/config.mk
@@ -216,7 +216,7 @@ LDFLAGS = \
         -Wl,-e,_trampoline
 
 # Pre- and post-build commands
-PREBUILD_CMD = # echo 'const char git_version[] = "$(shell git describe --tags | cut -f1 -d'-')-$(shell git describe --always --dirty)";' > gitversion.c
+PREBUILD_CMD =
 POSTBUILD_CMD =
 
 ansible: all
