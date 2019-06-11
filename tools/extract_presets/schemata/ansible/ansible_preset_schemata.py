6352d2974a7bed7731a912547d67c97925481ffb
Sam Boling <charles.samuel.boling@gmail.com>
2019-06-10 19:58:04 -0600

    remove duplicate/old extract_presets program

--- tools/extract_presets/schemata/ansible/ansible_preset_schemata.py
+++
@@ -1,9 +0,0 @@
-from schemata.ansible.v161 import AnsiblePresetSchema_v161
-from schemata.ansible.v161_es import AnsiblePresetSchema_v161_es
-
-
-ANSIBLE_PRESET_SCHEMATA = {
-    '1.6.1': AnsiblePresetSchema_v161,
-    '1.6.1-es': AnsiblePresetSchema_v161_es,
-}
-
